0025414035eda351a862099a3bd788fb41fe5502
igorz <ihorzviahin@gmail.com>
2025-07-11 22:39:27 +0200

    star1.3

---
+++ src/router.tsx
@@ -0,0 +1,21 @@
+import {createBrowserRouter, Navigate} from "react-router-dom";
+
+import {MainLayout} from "./layouts";
+import {ErrorPage, MovieDetailsPage, MoviesPage} from "./pages";
+
+const router = createBrowserRouter([
+    {
+        path: "", element: <MainLayout/>, errorElement: <ErrorPage/>, children: [
+
+            {index: true, element: <Navigate to={"movies"}/>},
+            {
+                path: 'movies', element: <MoviesPage/>,
+            },
+            {
+                path: "movies/:id", element: <MovieDetailsPage/>
+            }
+        ]
+    }
+]);
+
+export {router}
